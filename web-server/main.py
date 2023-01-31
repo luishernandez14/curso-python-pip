186436380a5d07cd3271e4c0473c3695bd64b092
luishernandez14 <alejolahr14@gmail.com>
2023-01-31 11:12:13 -0500

    Creating a server using FastApi and Uvicorn added

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI() #Creating an instance of an application
+
+@app.get('/') #Decorator syntax: Specifying route for getting in 
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse) #Adding another route
+def get_list():
+    return """
+        <h1>Hola soy una página</h1>
+        <p>soy un parrafo</p>
+
+    """  
+
 
 def run():
     store.get_categories()
